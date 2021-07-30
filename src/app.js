d2bba1de92009c7d7f20557d6e2db03987fafa6c
karinagiuseppina <karina.giuseppina@gmail.com>
2021-07-30 17:30:26 +0000

    done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extensions = [".io", ".com", ".net"];
+  let links = "";
+
+  for (let pronoun in pronouns) {
+    let pronounSelect = pronouns[pronoun];
+    for (let adj in adjs) {
+      let adjSelect = adjs[adj];
+      for (let noun in nouns) {
+        let nounSelect = nouns[noun];
+        for (let ext in extensions) {
+          let extSelect = extensions[ext];
+          links += pronounSelect + adjSelect + nounSelect + extSelect + "<br>";
+        }
+      }
+    }
+  }
+  let element = document.getElementById("text");
+  element.innerHTML = links;
 };
